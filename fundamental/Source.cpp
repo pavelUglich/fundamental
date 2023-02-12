0b4182d8d5ee47e0f1dd918bf09991c6a6191268
Pavel Uglich <puglich@inbox.ru>
2023-02-12 20:08:29 +0300

    Add project files.

---
+++ fundamental/Source.cpp
@@ -0,0 +1,24 @@
+﻿#include "OdeSolver.h"
+#include <iostream>
+
+
+int main() {
+	setlocale(0, "");
+	OdeSolver<double> odeSolver = {
+		{
+			[](double x, const std::vector<double>& y) {return y[1]; },
+			[](double x, const std::vector<double>& y) {return -y[0]; }
+		},
+		0.1e-6,
+		RUNGE_KUTTA_FELDBERG
+	};
+	const auto sol = odeSolver.solve(0, 1, { 0,1 });
+	for (auto x : sol)
+	{
+		std::cout << x << std::endl;
+	}
+	std::cout << "Аналитическое решение:" << std::endl;
+	std::cout << sin(1.0) << " " << cos(1.0) << std::endl;
+	system("pause");
+
+}
